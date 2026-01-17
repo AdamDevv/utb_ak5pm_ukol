f574215c1b5aad04541aa440447a4eeaa81005d8
Adam Prokop <adam.prokop1@seznam.cz>
2026-01-17 14:33:59 +0100

    feat: initial app layout

---
+++ lib/pages/favourites_page.dart
@@ -0,0 +1,20 @@
+﻿import 'package:flutter/material.dart';
+
+class FavouritesPage extends StatefulWidget {
+  const FavouritesPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FavouritesPageState();
+}
+
+class _FavouritesPageState extends State<FavouritesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: const Text('Favorites'),
+      ),
+      body: const Text("Favorites Page"),
+    );
+  }
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,20 @@
+﻿import 'package:flutter/material.dart';
+
+class GamesPage extends StatefulWidget {
+  const GamesPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _GamesPageState();
+}
+
+class _GamesPageState extends State<GamesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: const Text('Games'),
+      ),
+      body: const Text("Home Page"),
+    );
+  }
+}
